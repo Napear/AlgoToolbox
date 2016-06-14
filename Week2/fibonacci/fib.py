782e81f3743061023b1f42de1a17d60d7d121993
Napear <napear@gmail.com>
2016-06-14 07:53:12 -0700

    Code formating changes

--- Week2/fibonacci/fib.py
+++ Week2/fibonacci/fib.py
@@ -12,9 +12,10 @@ def fast_fib(n):
     fib_list = [0, 1]
     if n <= 1:
         return fib_list[n]
-    for i in range(2, n+1):
-        fib_list.append(fib_list[i-1]+fib_list[i-2])
+    for i in range(2, n + 1):
+        fib_list.append(fib_list[i - 1] + fib_list[i - 2])
     return fib_list[n]
 
+
 n = int(input())
-print("F"+str(n)+" = " + str(fast_fib(n)))
+print("F" + str(n) + " = " + str(fast_fib(n)))
